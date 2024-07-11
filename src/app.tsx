d9d5158eaea2b91996fcfbc1dff10573f32267cd
vbuarque <viniciusbuarque17@gmail.com>
2024-07-11 19:11:05 -0300

    Commit: primeiro commit

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <p className="zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+        <div className="h-16 bg-zinc-900 p-4 rounded-xl flex items-center">
+            <input type="text" placeholder="Para onde você vai?"/>
+            <input type="text" placeholder="Quando?"/>
+
+            <button>Continuar</button>
+        </div>
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade.</a></p>
+      </div>
+    </div>
+  )
+}
